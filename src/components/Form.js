52d989518a279461b4add8302f3d20962fa5f041
Kim Guyton <kimguyton@gmail.com>
2018-09-02 17:36:31 -0600

    working state

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,25 +5,23 @@ class Form extends Component{
     constructor(props){
     super(props)
         this.state = {
-            firstname: '',
-            lastname: '',
-            textArea: '',
-           
+            firstname: ' ',
+            lastname: ' ',
+            textArea: ' ',
+            
         }
         this.handleClick = this.handleClick.bind(this)
     }
     
         
     handleClick(){
-        this.setState({
-            firstname: ' ',
-            lastname: ' ',
-            textArea: ' '
-        })
+      
+        
     }
             
 
     render(){
+        
        return (        
         <div className='container'>
          <form>
@@ -31,16 +29,18 @@ class Form extends Component{
             onChange={ e => this.setState(({ firstname: e.target.value }))} placeholder='first name'/>
            <input 
              onChange={ e => this.setState(({ lastname: e.target.value }))} placeholder='last name'/>
-           <textarea className="textarea" placeholder='my thoughts...' 
-             onChange={ e => (this.setState({ text: e.target.value }))}>
-          </textarea>     
-            <button onClick={console.log(this.handleClick)}>
+           <textarea type='text' className="textarea" placeholder='my thoughts...' 
+             onChange={ e => console.log((this.setState({ textArea: e.target.value })))}>
+            </textarea>
+            <button onClick={() => this.handleClick(console.log(this.state))}>
             save
+           
          </button>
       </form>
      </div> 
     )
   }
+  
 }
 
 export default Form
